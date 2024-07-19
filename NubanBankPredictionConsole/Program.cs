2110ad41701454265ef4c855db0dc403a7e7d79e
Komolafe Olorundara <john.dhara@gmail.com>
2024-07-19 12:49:25 +0100

    Include test console

---
+++ NubanBankPredictionConsole/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NubanBankPredictionLib;
+
+Console.WriteLine("Hello, World!");
+
+
+var banks = await BankService.GetPossibleBanks("0263874788");
+
+foreach (var bank in banks)
+{
+    Console.WriteLine(bank.Name);
+}
+
+
+Console.ReadKey();
